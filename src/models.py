caffcd428a1e4163ebe35ca5806cba436b7bef6a
Francesc Font Clos <francesc.font@gmail.com>
2018-11-22 19:20:55 +0100

    changed simple CNN

--- src/models.py
+++ src/models.py
@@ -1,36 +1,30 @@
 import torch.nn as nn
 
-# Simple CNN taken from https://github.com/MorvanZhou/PyTorch-Tutorial/blob/master/tutorial-contents/401_CNN.py
-
 
 class CNN(nn.Module):
+    """
+    Simple CNN
+    taken from https://medium.com/ml2vec/intro-to-pytorch-with-image-classification-on-a-fashion-clothes-dataset-e589682df0c5
+
+    """
+
     def __init__(self):
         super(CNN, self).__init__()
-        self.conv1 = nn.Sequential(         # input shape (1, 224, 224)
-            nn.Conv2d(
-                in_channels=1,              # input height
-                out_channels=16,            # n_filters
-                kernel_size=5,              # filter size
-                stride=1,                   # filter movement/step
-                # if want same width and length of this image after Conv2d, padding=(kernel_size-1)/2 if stride=1
-                padding=2,
-            ),                              # output shape (16, 224, 224)
-            nn.ReLU(),                      # activation
-            # choose max value in 2x2 area, output shape (16, 112, 112)
-            nn.MaxPool2d(kernel_size=2),
-        )
-        self.conv2 = nn.Sequential(         # input shape (16, 112, 112)
-            nn.Conv2d(16, 32, 5, 1, 2),     # output shape (32, 112, 112)
-            nn.ReLU(),                      # activation
-            nn.MaxPool2d(2),                # output shape (32, 56, 56)
-        )
-        # fully connected layer, output 10 classes
-        self.out = nn.Linear(32 * 56 * 56, 10)
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=5, padding=2),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=5, padding=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2))
+        self.fc = nn.Linear(7*7*32, 10)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        # flatten the output of conv2 to (batch_size, 32 * 56 * 56)
-        x = x.view(x.size(0), -1)
-        output = self.out(x)
-        return output, x    # return x for visualization
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out
